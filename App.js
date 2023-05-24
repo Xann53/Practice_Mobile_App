18123c7fd4c8e486867be3782c0f21ddd4d0bb69
Xannetz Suficiencia <xvsuficiencia@gmail.com>
2023-05-24 23:05:47 +0800

    Had some practice with react native

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, Button, Alert, Platform, StatusBar, Dimensions } from 'react-native';
 
 export default function App() {
+
+  const pressImage = () => console.log("Image has been pressed!")
+
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container/*, styles.imageSize*/]}>
+      {/*<Text>Hello World! Futurama is back, baby! Really really long text. I wanna keep adding more texrt just to see what happens.</Text>
+      <TouchableOpacity onPress={pressImage}>
+        <Image
+          style={styles.imageSize}
+          blurRadius={5}
+          fadeDuration={1000}
+          source={{
+          uri: 'https://asia.olympus-imaging.com/content/000107506.jpg'
+        }} />
+      </TouchableOpacity>*/}
+      <Button color="red" title="click me"
+        onPress={() =>
+          Alert.alert("Button Press Alert", "Sup", [
+          { text: "Agree", onPress: () => console.log("He's a dude bro!") },
+          { text: "Disagree", onPress: () => console.log("He's not a dude bro!") }
+          ])
+        } />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue',
+    /*alignItems: 'center',
+    justifyContent: 'center',*/
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
+  imageSize: {
+    width: 300,
+    height: 200
+  }
 });
